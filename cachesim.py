e6ab62bdb786f0ee8d761b01cd7ac6595bcbc9bc
EilidhHendry <ehendry91@gmail.com>
2014-03-20 16:44:23 +0000

    adding commenting and tidying up

--- cachesim.py
+++ cachesim.py
@@ -16,26 +16,30 @@ class Cache(object):
         self.index_size = int(log(self.n_blocks, 2))
         self.tag_size = address_size - self.offset_size - self.index_size
 
-        # self.n_trace = get_count(testfile)
-
         # miss rates
         self.r_misses = 0
         self.w_misses = 0
 
+        # number of read/write operations
         self.n_read = 0
         self.n_write = 0
 
         self.cache = {}
 
     def decode(self, address):
+        ''' Method to decode the raw memory address into it's tag, index and offset.
+        '''
         bits = bin(int(address, 16))  # get the address in bits
         bits = bits.zfill(self.address_size)  # add padding until its the correct size
-        offset = bits[-self.offset_size:]
-        tag = bits[:self.tag_size]
-        index = bits[self.tag_size:self.index_size + self.tag_size]
+        offset = bits[-self.offset_size:]  # find the offset bits
+        tag = bits[:self.tag_size]  # find the tag bits 
+        index = bits[self.tag_size:self.index_size + self.tag_size]  # find the index bits
         return tag, index, offset
 
+    # TODO change to miss?
     def store(self, address):
+        ''' Method that handles a cache miss. It increments the lru values and removes the lru block if the set is full.
+        '''
         tag, index, offset = self.decode(address)
         # returns the cache set at the index or creates an empty dict at the index if not there
         cache_set = self.cache.setdefault(index, {})
@@ -48,7 +52,9 @@ class Cache(object):
             else:
                 cache_set[tag] = 1
         else:
+            # the lru block is the block with the min lru value
             lru = min(cache_set.iterkeys(), key=lambda k: cache_set[k])
+            # delete the lru block
             del cache_set[lru]
             # if the cache set is not empty then add 1 to the value
             if tag in cache_set:
@@ -56,10 +62,26 @@ class Cache(object):
             # if the cache set is empty then set the tag value to 1
             else:
                 cache_set[tag] = 1
+        # Checks the length of the cache set is less than or equal to the no of ways
 	assert len(cache_set) <= self.n_ways
 
 
+    def increment_lru(self, address):
+        ''' Method to increase the lru value of a block on the event of a hit.
+        '''
+        tag, index, offset = self.decode(address)
+        cache_set = self.cache[index]
+        if len(cache_set) < self.n_ways:
+            	# if the cache set is not empty then add 1 to the value
+            if tag in cache_set:
+                cache_set[tag] += 1
+            	# if the cache set is empty then set the tag value to 1
+            else:
+                cache_set[tag] = 1
+
     def is_cache_hit(self, address):
+        ''' Method to check if an address is already stored in the cache (a hit).
+        '''
         tag, index, offset = self.decode(address)
         if index in self.cache:
             cache_set = self.cache[index]
@@ -68,8 +90,10 @@ class Cache(object):
         return False
 
     def read_write(self, op, address):
+        ''' Method
+        '''
         if self.is_cache_hit(address):
-            pass
+            self.increment_lru(address)
         else:
             self.store(address)
             # Check if the number of items currently in the cache is less than the number of sets in the cache
@@ -84,44 +108,42 @@ class Cache(object):
         elif op == 'W':
             self.n_write += 1
 
-	def _reset(self):
-		self.cache = {}
-        	self.n_read = 0
-        	self.n_write = 0
-        	self.r_misses = 0
-        	self.w_misses = 0
+    def _reset(self):
+        self.cache = {}
+        self.n_read = 0
+        self.n_write = 0
+        self.r_misses = 0
+        self.w_misses = 0
 
     def simulate(self, filename):
-
-        # if not len(sys.argv) == 5: # Check for command line arguments
-        #     print "Usage: %s [ cache size ] [ n-sets ] [ block size ] [ filename ]\n" % \
-        #         os.path.basename(sys.argv[0])
-        #     sys.exit(0)
-        #
-        # Cache.cache_size = 4  # int(sys.argv[1])
-        # Cache.n_sets = 1  # int(sys.argv[2])
-        # Cache.block_size = 32 # int(sys.argv[3])
-        #filename = 'test.out'  # int(sys.argv[4])
-
+        '''
+        '''
+        # Checks if the cache is empty and resets it if it's not.
 	if self.cache:
 		self._reset()
 
+        # Takes the tracefile and gets a list of tuples of the operation and address.
         tracelist = get_trace(filename)
-
+        
+        # Loops through the trace list and 
+        # Also checks that the length of the cache is never bigger than it should be
         for (op, address) in tracelist:
         	self.read_write(op, address)
 		assert len(self.cache) <= self.n_blocks
-
+        
+        # Calculate result values
         tot_misses = self.r_misses + self.w_misses
         tot_ops = self.n_write + self.n_read
         total_missrate = tot_misses / (tot_ops * 1.0)
         write_missrate = self.w_misses / (self.n_write * 1.0)
         read_missrate = self.r_misses / (self.n_read * 1.0)
 
+        #REMOVE
 	assert all([len(s) <= self.n_ways
                    for s in self.cache.itervalues()])
         assert tot_ops == len(tracelist)
 
+        # Dictionary containing the results
         results = dict(
             cache_size=self.cache_size / 1024,
             n_ways=self.n_ways,
@@ -133,19 +155,9 @@ class Cache(object):
         )
         return results
 
-        # print "total missrate: ", total_missrate
-        # print "write missrate: ", write_missrate
-        # print "read missrate: ", read_missrate
-        # print "total operations: ", tot_ops
-        # print "r_misses: ", self.r_misses
-        # print "w_misses: ", self.w_misses
-        # print "n_read: ", self.n_read
-        # print "n_write: ", self.n_write
-        # print "count: ", get_count(filename)
-
 
 def run():
-    simulate_all(gcc, 'testout.json')
+    simulate_all('mcf_memref.out', 'testout.json')
 
 
 def test_caches():
--- parse.py
+++ parse.py
@@ -2,17 +2,9 @@ from collections import defaultdict
 
 test = 'test.out'
 
-def getRW(line):
+def get_rw(line):
     '''Function to get R or W from the instruction.'''
     return line[0]
-
-def getBinBool(line):
-    '''Function to get the 0 or 1 bit from a line and make it a boolean.'''
-    value = line[-2]
-    if value == '1':
-        return True
-    else:
-        return False
 
 def get_address(line):
     '''Function to get the address from a line.'''
@@ -21,22 +13,4 @@ def get_address(line):
 
 def get_trace(filename):
     with open(filename) as tfile:
-        return [(getRW(line), get_address(line)) for line in tfile]
-
-
-def getDict(filename):
-    '''Function to create a dictionary from the file containing the instructions with the addresses
-    as the keys and the bits (converted to booleans) as values.'''
-    dicts = defaultdict(list)
-    with open(filename) as tfile:
-        for line in tfile:
-            dicts[get_address(line)].append(getBinBool(line))
-    return dicts
-
-def get_count(filename):
-    '''Returns the number of instructions in a file.'''
-    count = 0.0
-    with open(filename) as tfile:
-        for line in tfile:
-            count+=1
-    return count
+        return [(get_rw(line), get_address(line)) for line in tfile]
